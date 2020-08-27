52eb64af4632d909d1835f8cc6249df73132acd7
Mitchell Meier <mdmfvz@mst.edu>
2020-08-27 11:11:27 -0500

    fix infinite loop error when clicking an empty gif for fbf, as well as added credit for new contributor

--- __init__.py
+++ __init__.py
@@ -394,5 +394,5 @@ if __name__ == "__main__":
 
     #change to commented line if debugging
     #app.run(host='0.0.0.0', debug=True)
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
 
